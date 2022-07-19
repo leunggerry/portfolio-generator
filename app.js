7cc343f22896a914fafe2c94fa6978501dbf4c03
gigi.upNorth <leung.gerry@gmail.com>
2022-07-19 17:20:35 -0400

    [node] adding inquirer module for user prompts

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 /** Modules
  **************************************************************************************************/
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-/** Global Constants
- **************************************************************************************************/
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// const fs = require("fs");
+const inquirer = require("inquirer");
+// const generatePage = require("./src/page-template.js");
+// /** Global Constants
+//  **************************************************************************************************/
+// const pageHTML = generatePage(name, github);
 
-/** Functions
- **************************************************************************************************/
-// const printProfileData = (profileDataArr) => {
-//   profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
+// /** Functions
+//  **************************************************************************************************/
 
-// printProfileData(profileDataArgs);
+// /** Main Program
+//  **************************************************************************************************/
+// fs.writeFile("index.html", generatePage(name, github), (err) => {
+//   if (err) throw err;
 
-/** Main Program
- **************************************************************************************************/
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
-
-  console.log("Portfolio complete! Check out index.html to see output");
-});
+//   console.log("Portfolio complete! Check out index.html to see output");
+// });
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
