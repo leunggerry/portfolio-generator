f1c434fbea3752fd9e4cf528271e9f871cfe9552
gigi.upNorth <leung.gerry@gmail.com>
2022-07-19 16:23:39 -0400

    [Node] Generating HTML file
    Description:
      - modularizing the files
      - separate the html generation

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+/** Modules
+ **************************************************************************************************/
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+/** Global Constants
+ **************************************************************************************************/
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 /** Functions
  **************************************************************************************************/
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+// const printProfileData = (profileDataArr) => {
+//   profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+/** Main Program
+ **************************************************************************************************/
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+  if (err) throw err;
+
+  console.log("Portfolio complete! Check out index.html to see output");
+});
